ccaa76e9a6bb33279d39e62f834dcfddf1a56ebd
Evgeniya Kugaevskaya <butka_e@mail.ru>
2018-05-31 01:17:09 +0500

    Урок 1. Усложненный. 2 версия

--- lesson1/lesson1_reg/js/lesson1.js
+++ lesson1/lesson1_reg/js/lesson1.js
@@ -1,20 +1,17 @@
-var finansLimit = prompt("Ваш бюджет на месяц");
-var nameShop = prompt("Название Вашего магазина");
-var employer = {	
+var finansLimit = +prompt("Ваш бюджет на месяц")
+	nameShop = prompt("Название Вашего магазина"),
+	mainList = {
+		finans: finansLimit,
+		shop: nameShop,
+		shopGoods: [],
+		employers: {},
+		open: false
+	}
+
+for(let i = 0; i < 3; i++) {
+    mainList.shopGoods[i] = prompt("Какой тип товаров будем продавать?");
 }
 
-var mainList = {
-	finans: finansLimit,
-	shop: nameShop,
-	shopGoods: [],
-	employers: employer,
-	open: false
-}
-
-mainList.shopGoods[0] = prompt("Какой тип товаров будем продавать?");
-mainList.shopGoods[1] = prompt("Какой тип товаров будем продавать?");
-mainList.shopGoods[2] = prompt("Какой тип товаров будем продавать?");
-
-alert('Ваш бюджет на день составит ' + Math.round(parseFloat(mainList.finans/30) * 100) / 100 + ' у.е.');
+isNaN(mainList.finans) ? alert('Вы ввели некорректный бюджет') : alert('Ваш бюджет на день составит ' + (mainList.finans/30).toFixed(2) + ' у.е.');
 
 console.log(mainList);
