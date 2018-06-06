9c32cf913bc3ea26baddb44a7b4a045c7b2aa9a9
Evgeniya Kugaevskaya <butka_e@mail.ru>
2018-06-07 01:21:44 +0500

    Основное. Добавлен 5 урок (часть 1)

---
+++ lesson5_1_reg/js/script.js
@@ -0,0 +1,20 @@
+let menuItem = document.querySelectorAll('.menu-item');
+let menu = document.querySelector('.menu');
+
+menu.insertBefore(menuItem[2], menuItem[1]);
+
+let menuItemFive = document.createElement('li');
+menuItemFive.classList.add('menu-item');
+menuItemFive.textContent = "Пятый пункт";
+menu.appendChild(menuItemFive);
+
+document.body.style.backgroundImage = 'url(../img/apple_true.jpg)';
+
+let title = document.getElementById('title');
+title.textContent = 'Мы продаем только подлинную технику Apple';
+
+let adv = document.querySelector('.adv');
+adv.remove();
+
+let promp = document.getElementById('prompt');
+promp.textContent = prompt('Ваше отношение к технике Apple','');
