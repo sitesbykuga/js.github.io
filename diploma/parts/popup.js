3b0c741c3034711373b503f2f35c7298580e3a48
Evgeniya Kugaevskaya <butka_e@mail.ru>
2018-07-01 03:31:30 +0500

    Изменен модуль popup: добавлена возможность скрытия кнопки вызова

--- diploma/parts/popup.js
+++ diploma/parts/popup.js
@@ -1,13 +1,25 @@
-function popup (element, btnClassName) {
+function popup (element, btnClassName, status=false) {
 	let popupWin = element
 		;
 
 	function showPopup(){
 		popupWin.style.display = 'block';
+		if (status) {
+			let btn = document.getElementsByClassName(btnClassName);
+			for (let i = 0; i < btn.length; i++){
+				btn[i].style.display = 'none';
+			}
+		}
 	}
 
 	function closePopup(){
-		popupWin.style.display = 'none';		
+		popupWin.style.display = 'none';	
+		if (status) {
+			let btn = document.getElementsByClassName(btnClassName);
+			for (let i = 0; i < btn.length; i++){
+				btn[i].style.display = 'inline';
+			}
+		}	
 	}
 
 	document.body.addEventListener('click', (event) => { 
--- diploma/parts/popups.js
+++ diploma/parts/popups.js
@@ -4,7 +4,7 @@ function popups() {
 	
 	popup(document.getElementsByClassName('popup-design')[0], 'button-design');
 	popup(document.getElementsByClassName('popup-consultation')[0], 'button-consultation');
-	popup(document.getElementsByClassName('popup-gift')[0], 'fixed-gift');
+	popup(document.getElementsByClassName('popup-gift')[0], 'fixed-gift', true);
 };
 
 module.exports = popups;
