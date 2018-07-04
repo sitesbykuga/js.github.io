7729c226ffc6856dce3dc79cc7fcbe30ada3b260
Evgeniya Kugaevskaya <butka_e@mail.ru>
2018-07-04 10:26:51 +0500

    Добавление: подгрузка блоков

--- diploma/js/script.js
+++ diploma/js/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 	var slider = require('../parts/slider.js')
 		, popups = require('../parts/popups.js')
-		, forms = require('../parts/forms.js')		
+		, forms = require('../parts/forms.js')
+		, stylesList = require('../parts/stylesList.js')
 		;  
 	 
 	slider();		
 	popups();
 	forms();
+	stylesList();
 });
